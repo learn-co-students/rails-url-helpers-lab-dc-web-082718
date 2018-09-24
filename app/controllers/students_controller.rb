e815e6454c5a9080f01265976eb71cf3dc100601
kruchkamgar <kruchkamgar@gmail.com>
2018-09-23 20:23:38 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,12 +1,31 @@
 class StudentsController < ApplicationController
   before_action :set_student, only: :show
-  
+
   def index
     @students = Student.all
   end
 
   def show
+    # set_student
   end
+
+  def activate
+    student = set_student
+
+    # if student.active == false then student.active = true else student.active = false end
+    student.active = student.active ? false : true
+
+    student.save
+
+    redirect_to student_path(student)
+  end
+
+  # def activation
+  #   @student = set_student
+  #   @student.active = params[:activation]
+  #
+  #   redirect_to student_path(@student)
+  # end
 
   private
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,9 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
+Student.create!(first_name: "Daenerys", last_name: "Targaryen")
+Student.create!(first_name: "Lindsey", last_name: "Stirling")
+
 # Examples:
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
